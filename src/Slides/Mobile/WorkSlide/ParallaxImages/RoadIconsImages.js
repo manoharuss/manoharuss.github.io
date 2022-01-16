0322a77cbd00a5c126ae612ad51f1313850668d1
manoharuss <manoharuss@gmail.com>
2022-01-17 00:30:46 +0530

    more

--- src/Slides/Mobile/WorkSlide/ParallaxImages/RoadIconsImages.js
+++ src/Slides/Mobile/WorkSlide/ParallaxImages/RoadIconsImages.js
@@ -26,7 +26,7 @@ const IconsInspect = styled.img.attrs({
 transition: transform 0.2s ease-out;
 bottom:-110vh;
 transform-origin: left;
-left:6vw;
+left:8vw;
 position: absolute;
 /* border: 1px dashed red; */
 height: 80vh;
